e0e150382acf6abe907bbcc74946005af6244486
Eric Lopez <hello@ericlopez.me>
2020-05-30 14:01:45 +0200

    parse telegram payload as webhook input

--- function/webhook.go
+++ function/webhook.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -9,9 +15,37 @@ func main() {
 	lambda.Start(handler)
 }
 
-func handler(_ events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, World",
-	}, nil
+type request struct {
+	Message struct {
+		Date int64 `json:"date"`
+		From struct {
+			ID int `json:"id"`
+		} `json:"from"`
+		Text string `json:"text"`
+	} `json:"message"`
+}
+
+func handler(r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var req request
+	if err := json.NewDecoder(bytes.NewBufferString(r.Body)).Decode(&req); err != nil {
+		return response(http.StatusInternalServerError, err)
+	}
+
+	log.Printf(
+		"from %d at %s: %s",
+		req.Message.From.ID,
+		time.Unix(req.Message.Date, 0),
+		req.Message.Text,
+	)
+
+	return response(http.StatusOK, nil)
+}
+
+func response(code int, err error) (*events.APIGatewayProxyResponse, error) {
+	resp := &events.APIGatewayProxyResponse{StatusCode: code}
+	if err != nil {
+		resp.Body = err.Error()
+	}
+
+	return resp, nil
 }
