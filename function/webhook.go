e22947324cb97b61280da0516d870e45fee455e3
Eric Lopez <hello@ericlopez.me>
2020-05-30 15:59:49 +0200

    check the given location

--- function/webhook.go
+++ function/webhook.go
@@ -3,19 +3,22 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-const acceptedMembersEnvVar = "ACCEPTED_MEMBERS"
+const (
+	acceptedMembersEnvVar   = "ACCEPTED_MEMBERS"
+	acceptedLocationsEnvVar = "ACCEPTED_LOCATIONS"
+)
 
 type request struct {
 	Message struct {
@@ -35,28 +38,48 @@ func handler(r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, 
 	var req request
 	decoder := json.NewDecoder(bytes.NewBufferString(r.Body))
 	if err := decoder.Decode(&req); err != nil {
-		return response(http.StatusInternalServerError, err)
+		return response(http.StatusBadRequest, err)
+	}
+
+	if req.Message.Text == "/start" {
+		return response(http.StatusOK, nil)
 	}
 
 	acceptedMembers := os.Getenv(acceptedMembersEnvVar)
 	memberExtractor := regexp.MustCompile(`([a-z]+):(\d+)`)
 	if !memberExtractor.MatchString(acceptedMembers) {
-		log.Fatalf("invalid member list %s", acceptedMembers)
+		log.Printf("invalid member list %s", acceptedMembers)
+		return response(http.StatusOK, nil)
 	}
 
-	matches := memberExtractor.FindAllStringSubmatch(acceptedMembers, -1)
-	members := make(map[int]string, len(matches))
-	for _, match := range matches {
+	memberMatches := memberExtractor.FindAllStringSubmatch(acceptedMembers, -1)
+	members := make(map[int]string, len(memberMatches))
+	for _, match := range memberMatches {
 		chatID, err := strconv.Atoi(match[2])
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("invalid chat ID %s", match[2])
+			return response(http.StatusOK, nil)
 		}
 		members[chatID] = match[1]
 	}
 
 	name, allowed := members[req.Message.From.ID]
 	if !allowed {
-		return response(http.StatusBadRequest, errors.New("unrecognized chat"))
+		log.Printf("unrecognized chat ID %d", req.Message.From.ID)
+		return response(http.StatusOK, nil)
+	}
+
+	locations := strings.Split(os.Getenv(acceptedLocationsEnvVar), " ")
+	var allowedLocation bool
+	for _, location := range locations {
+		if location == req.Message.Text {
+			allowedLocation = true
+			break
+		}
+	}
+	if !allowedLocation {
+		log.Printf("unrecognized location %s", req.Message.Text)
+		return response(http.StatusOK, nil)
 	}
 
 	log.Printf("from %s at %s: %s", name, time.Unix(req.Message.Date, 0), req.Message.Text)
